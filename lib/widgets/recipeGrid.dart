c92768b77402326a871aa0b857749c18a2902ed8
koboolean <koboolean@gmail.com>
2023-03-19 14:04:34 +0900

    GridView 일치율 관련 및 Info 관련 아이콘 Commit

--- lib/widgets/recipeGrid.dart
+++ lib/widgets/recipeGrid.dart
@@ -7,9 +7,12 @@ Widget recipeGrid(
     required Function callbackFunction}) {
   return GridView.count(
     shrinkWrap: true,
-    padding: const EdgeInsets.fromLTRB(10, 10, 10, 20), // padding
-    crossAxisCount: 2, // 한 열에 보여줄 갯수
-    childAspectRatio: 2/1.6, // 상하비율
+    padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+    // padding
+    crossAxisCount: 2,
+    // 한 열에 보여줄 갯수
+    childAspectRatio: 2 / 1.7,
+    // 상하비율
     children: List.generate(datas.length, (index) {
       var data = datas[index];
       var match = data["match"]!;
@@ -20,26 +23,42 @@ Widget recipeGrid(
           },
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Container(
-                margin: new EdgeInsets.all(3.0), // margin
-                height: 100,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(data["image"]!),
-                        fit: BoxFit.fill))),
-            (!type)
-                ? Text(
-                    data["title"]!,
-                    style: TextStyle(fontSize: 16),
-                    overflow: TextOverflow.fade,
-                    maxLines: 2,
-                  )
-                : Text(
-                    "${data["title"]!}\n($match%)",
-                    style: TextStyle(fontSize: 16),
-                    overflow: TextOverflow.fade,
-                    maxLines: 2,
-                  )
+            Stack(
+              children: [
+                Container(
+                    margin: EdgeInsets.all(3.0), // margin
+                    height: 100,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(data["image"]!),
+                            fit: BoxFit.fill))),
+                (type)
+                    ? Positioned(
+                              top: 5,
+                              right: 5,
+                              child: GestureDetector(
+                                onTap: () {
+                                  // 정보보기
+                                },
+                                child: Icon(Icons.info, color : Colors.black38),
+                              ))
+                    : Text("")
+                , Positioned(
+                    bottom: 5,
+                    right: 5,
+                    child: Text(
+                      "일치율 : ($match%)",
+                      style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, backgroundColor: Colors.white30),
+                    ))
+                ,
+              ],
+            ),
+            Text(
+              data["title"]!,
+              style: TextStyle(fontSize: 16),
+              overflow: TextOverflow.fade,
+              maxLines: 1,
+            )
           ]));
     }),
   );
