f42c8ac3bebfd3b1c4b9481fd5f873eff3bb8bb9
koboolean <koboolean@gmail.com>
2023-03-20 22:58:11 +0900

    TabBarView 관련사항 Commit

--- lib/screens/myRefriger.dart
+++ lib/screens/myRefriger.dart
@@ -12,133 +12,163 @@ class MyFridge extends StatefulWidget {
   State<MyFridge> createState() => _MyFridgeState();
 }
 
-class _MyFridgeState extends State<MyFridge> {
+class _MyFridgeState extends State<MyFridge> with TickerProviderStateMixin {
   TextEditingController jobController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final authService = context.read<AuthService>();
     final user = authService.currentUser()!;
-    final ValueNotifier<String> version =
-        ValueNotifier<String>("1.0"); // ValueNotifier 변수 선언
 
-    return Consumer(
-      builder: (context, bucketService, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("냉장고 관리"),
-          ),
-          body: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                      width: 230,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Material Search",
-                          ),
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w200),
-                        ),
-                      )),
-                  Container(
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: THEME_COLOR,
-                        style: BorderStyle.solid,
-                      ),
-                    ),
+    TabController? _tabController;
+
+    @override
+    void initState() {
+      _tabController = TabController(length: 2, vsync: this);
+      super.initState();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("냉장고 관리"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                    width: 230,
                     child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 17, horizontal: 18),
-                      child: Text(
-                        "+",
-                        style: TextStyle(
-                          fontSize: 20,
+                      padding: EdgeInsets.symmetric(),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Material Search",
                         ),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w200),
+                      ),
+                    )),
+                Container(
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: THEME_COLOR,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 17, horizontal: 18),
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
                     ),
                   ),
-                  Container(
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
+                ),
+                Container(
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: THEME_COLOR,
+                    border: Border.all(
                       color: THEME_COLOR,
-                      border: Border.all(
-                        color: THEME_COLOR,
-                        style: BorderStyle.solid,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 17, horizontal: 5),
+                    child: Text(
+                      "add",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
                     ),
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 17, horizontal: 5),
-                      child: Text(
-                        "add",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height-120,
+            child: DefaultTabController(
+              length: 2,
+              initialIndex: 0,
+              child: Column(
+                children: [
+                  TabBar(
+                    tabs: [
+                      Container(
+                        height: 30,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Tab1',
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(children: [
-                DefaultTabController(
-                  initialIndex: 1,
-                  length: 3,
-                  child: Scaffold(
-                    appBar: AppBar(
-                      title: const Text('TabBar Widget'),
-                      bottom: const TabBar(
-                        tabs: <Widget>[
-                          Tab(
-                            text: "전체",
-                          ),
-                          Tab(
-                            text: "유통기한 임박",
-                          ),
-                          Tab(
-                            text: "최근 차감된 재료",
-                          ),
+                      Container(
+                        height: 30,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Tab2',
+                        ),
+                      ),
+                    ],
+                    indicator: BoxDecoration(
+                      gradient: LinearGradient(
+                        //배경 그라데이션 적용
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.pinkAccent,
                         ],
                       ),
                     ),
-                    body: const TabBarView(
-                      children: <Widget>[
-                        Center(
-                          child: Text("ListView-1"),
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Colors.black,
+                    controller: _tabController,
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: [
+                        Container(
+                          color: Colors.yellow[200],
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Tab1 View',
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
                         ),
-                        Center(
-                          child: Text("ListView-2"),
-                        ),
-                        Center(
-                          child: Text("ListView-3"),
+                        Container(
+                          color: Colors.green[200],
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Tab2 View',
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ]),
-            ],
+                ],
+              ),
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
