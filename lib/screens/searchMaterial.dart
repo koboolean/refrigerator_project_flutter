8e76860a7c790f664f807d1610b3da68292f1378
namo <kimnamo0113@gmail.com>
2023-03-23 00:43:52 +0900

    searchMaterial(DropDownTextField) 적용 buttonGrid 작업예정

--- lib/screens/searchMaterial.dart
+++ lib/screens/searchMaterial.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,28 +13,83 @@ class SearchMaterial extends StatefulWidget {
   State<SearchMaterial> createState() => _SearchMaterialState();
 }
 
-class _SearchMaterialState extends State<SearchMaterial> with TickerProviderStateMixin {
-  TextEditingController jobController = TextEditingController();
+class _SearchMaterialState extends State<SearchMaterial>
+    with TickerProviderStateMixin {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  FocusNode searchFocusNode = FocusNode();
+  FocusNode textFieldFocusNode = FocusNode();
+  late SingleValueDropDownController _cnt;
+  late MultiValueDropDownController _cntMulti;
+
+  @override
+  void initState() {
+    _cnt = SingleValueDropDownController();
+    _cntMulti = MultiValueDropDownController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cnt.dispose();
+    _cntMulti.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final authService = context.read<AuthService>();
-    final user = authService.currentUser()!;
-
-    TabController? _tabController;
-
-    @override
-    void initState() {
-      _tabController = TabController(length: 2, vsync: this);
-      super.initState();
-    } // ValueNotifier 변수 선언
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("냉장고 관리"),
+        title: Text("재료 관리"),
       ),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                DropDownTextField(
+                  // initialValue: "name4",
+                  listSpace: 20,
+                  listPadding: ListPadding(top: 20),
+                  enableSearch: true,
+                  validator: (value) {
+                    if (value == null) {
+                      return "Required field";
+                    } else {
+                      return null;
+                    }
+                  },
+                  dropDownList: const [
+                    DropDownValueModel(name: 'name1', value: "value1"),
+                    DropDownValueModel(name: 'name2', value: "value2"),
+                    DropDownValueModel(name: 'name3', value: "value3"),
+                    DropDownValueModel(name: 'name4', value: "value4"),
+                    DropDownValueModel(name: 'name5', value: "value5"),
+                    DropDownValueModel(name: 'name6', value: "value6"),
+                    DropDownValueModel(name: 'name7', value: "value7"),
+                    DropDownValueModel(name: 'name8', value: "value8"),
+                  ],
+                  listTextStyle: const TextStyle(color: Colors.red),
+                  dropDownItemCount: 8,
+
+                  onChanged: (val) {},
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const SizedBox(
+                  height: 100,
+                  child: Text("지금 냉장고 재료", style: TextStyle(fontSize: 20)),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/buttonGrid.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+Widget buttonGrid(
+    {required List<Map<String, String>> datas,
+    required bool type,
+    required Function callbackFunction}) {
+  return GridView.count(
+    shrinkWrap: true,
+    padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+    // padding
+    crossAxisCount: 3,
+    // 한 열에 보여줄 갯수
+    childAspectRatio: 2 / 1.7,
+    // 상하비율
+    children: List.generate(datas.length, (index) {
+      var data = datas[index];
+      var match = data["match"]!;
+      return GestureDetector(
+          onTap: () {
+            // callbackFunction 호출하여 작업할까 고민중... (재사용 관련)
+            callbackFunction(index);
+          },
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Stack(
+              children: [
+                Container(
+                    margin: EdgeInsets.all(3.0), // margin
+                    height: 100,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(data["image"]!),
+                            fit: BoxFit.fill))),
+                Positioned(
+                    bottom: 5,
+                    right: 5,
+                    child: Text(
+                      "일치율 : ($match%)",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.white30),
+                    )),
+              ],
+            ),
+            Text(
+              data["title"]!,
+              style: TextStyle(fontSize: 16),
+              overflow: TextOverflow.fade,
+              maxLines: 1,
+            )
+          ]));
+    }),
+  );
+}
