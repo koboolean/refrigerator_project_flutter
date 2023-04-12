f8c496eb3c6f244960e31b9f637e3adcacc3d4d2
yang <taegeonyang@gmail.com>
2023-04-12 20:21:42 +0900

    헤더제거

--- lib/screens/myRecipeDetail.dart
+++ lib/screens/myRecipeDetail.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:refrigerator_project_flutter/model/recipeItem.dart';
-import 'package:refrigerator_project_flutter/model/myrecipeItem.dart';
-import 'package:refrigerator_project_flutter/services/auth_service.dart';
-import 'package:refrigerator_project_flutter/widgets/recipeGrid_One.dart';
-import 'package:refrigerator_project_flutter/widgets/recipeList.dart';
-import 'package:refrigerator_project_flutter/widgets/favoriteGrid.dart';
+// import 'package:flutter/cupertino.dart';
 
-import 'package:refrigerator_project_flutter/widgets/recipeGrid.dart';
+// import 'package:provider/provider.dart';
+// import 'package:refrigerator_project_flutter/model/recipeItem.dart';
+// import 'package:refrigerator_project_flutter/model/myrecipeItem.dart';
+// import 'package:refrigerator_project_flutter/services/auth_service.dart';
+
+// import 'package:refrigerator_project_flutter/widgets/recipeList.dart';
+// import 'package:refrigerator_project_flutter/widgets/favoriteGrid.dart';
+
+// import 'package:refrigerator_project_flutter/widgets/recipeGrid.dart';
+import 'package:flutter/material.dart';
 import 'package:refrigerator_project_flutter/screens/myRecipeAdd.dart';
+import 'package:refrigerator_project_flutter/widgets/recipeGrid_One.dart';
 
 class myRecipeDetail extends StatelessWidget {
   var list = [
